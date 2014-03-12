3ba03fc4e45a5d57d68c11fa9788b2a728c1606a
Cyblar <lars217@gmail.com>
2014-03-12 19:09:19 +0100

    Lagt til Utvikling_lars

---
+++ Prosjekt/Utvikling_lars/header.php
@@ -0,0 +1,31 @@
+<?php
+// include_once "";
+?>
+
+<!--HeaderBanner-->
+<div class="row headerContainer">
+  <div class="col span_12 logoSpace">
+    
+    <img id="mainLogo" src="Img/logoName.png"></img>
+    
+  <!--<div class="col span_4 loginSpace"><h3>LOGG INN / REGISTRER</h3></div>-->
+</div>
+<!--headerContainer_close-->
+
+<div class="row">
+  <ul class="col span_9 nav">
+    <li>
+      <a href="index.php">Hjem</a>
+    </li>
+    <li>
+      <a href="faq.php">Faq</a>
+    </li>
+    <li>
+      <a href="meldPa.php">Meld deg på et utvalg</a>
+    </li>
+  </ul>
+  <ul class="col span_3 nav">
+    <li>LOGG INN</li>
+    <li>REGISTRER DEG</li>
+  </ul>
+</div>
